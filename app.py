3183b350725ad56e70d4109fe20b61dc79d03fd1
Anthony Pinho <contato.anthony27@gmail.com>
2023-09-11 23:56:43 +0000

    Feat: Criadas variavéis e função para base de dados

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, abort
 from posts import posts
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pudim'
 
+app.config.from_object(__name__)
+DATABASE = "banco.db"
 
+def conectar():
+    return sqlite3.connect(DATABASE)
 
 @app.route('/')
 def exibir_entradas():

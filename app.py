899da098fcbaaa097618646480cd6a4917946d4b
Anthony Pinho <contato.anthony27@gmail.com>
2023-09-12 00:59:53 +0000

    Feat: Adicionando g e select

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, flash, redirect, url_for, abort
+from flask import Flask, render_template, g, request, session, flash, redirect, url_for, abort
 from posts import posts
 import sqlite3
 
@@ -6,15 +6,33 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pudim'
 
 app.config.from_object(__name__)
-DATABASE = "banco.db"
+DATABASE = "banco.bd"
 
 def conectar():
     return sqlite3.connect(DATABASE)
 
+@app.before_request
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
+
+
 @app.route('/')
 def exibir_entradas():
-    entradas = posts[::-1] # Mock das postagens
-    return render_template('exibir_entradas.html', entradas=entradas)
+    # entradas = posts[::-1] # Mock das postagens
+
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado = g.bd.execute(sql)
+
+    entrada = [
+        {"titulo": "Primeiro titulo", "texto":"Primeiro", "data_criacao":"11/09/2023"},
+        {"titulo": "Segundo titulo", "texto":"Segundo", "data_criacao":"12/09/2023"}
+    ]
+
+    return render_template('exibir_entradas.html', entradas=entrada)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -47,10 +65,10 @@ def inserir_entradas():
     else:
         return redirect(url_for('login'))
     
-@app.route('/posts/<int:id>')
-def exibir_entrada(id):
-    try:
-        entrada = posts[id-1]
-        return render_template('exibir_entrada.html', entrada= entrada)
-    except Exception:
-        abort(404)
+# @app.route('/posts/<int:id>')
+# def exibir_entrada(id):
+#     try:
+#         entrada = posts[id-1]
+#         return render_template('exibir_entrada.html', entrada= entrada)
+#     except Exception:
+#         abort(404)
